b55d7aec944dda608eb550947efb3afa14ef7ec7
Sunil Panchal <sunilpanchal123.sp@gmail.com>
2020-05-30 12:22:01 +0530

    file system and command// yargs 2

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const yargs = require('yargs')
 yargs.command({
     command:'add',
     describe:'Add a note',
-    handler:function(){
-        console.log('Adding a note...')
+    builder: {
+        title : {
+            describe:"Note title",
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        console.log('Adding a note...' + argv.title)
     }
 })
 yargs.command({
@@ -31,7 +38,8 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
+// console.log(yargs.argv)
 
 
 
