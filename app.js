c9a4ee619a6294cd834775957a8fc2bd002e347e
Sunil Panchal <sunilpanchal123.sp@gmail.com>
2020-05-30 10:47:10 +0530

    file system and commands //3 Argumentparsing with Yargs part i

---
+++ app.js
@@ -0,0 +1,79 @@
+const yargs = require('yargs')
+
+yargs.command({
+    command:'add',
+    describe:'Add a note',
+    handler:function(){
+        console.log('Adding a note...')
+    }
+})
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    handler:function(){
+        console.log('Removing a note...')
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'list of variable',
+    handler:function(){
+        console.log("This is our list")    
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'It can read the data',
+    handler: function(){
+        console.log("This can read the data of powershell")
+    }
+})
+
+console.log(yargs.argv)
+
+
+
+
+
+
+
+
+// const chalk = require('chalk')
+
+// const msg = "Success!"
+// console.log(chalk.green.bold(msg))
+
+// const par = process.argv[2]
+// console.log(par)
+
+
+
+
+
+
+
+
+
+// const validator = require('validator')
+
+
+// const getNotes = require('./notes.js')
+// const msg = getNotes()
+// console.log(msg)
+
+// const id = 'Sunilpanchal123.sp@gmail.com'
+// console.log(validator.isEmail(id))
+
+
+
+// const name = getNotes(4,2)
+// console.log(name)
+
+
+
+
+// const fs = require('fs')
+// fs.writeFileSync("notes.txt","This file is created by node js")
+// fs.appendFileSync("notes.txt","This is updating the file using node js")
---
+++ notes.js
@@ -0,0 +1,13 @@
+const getNotes = function(){
+    return "Your notes...."
+}
+
+
+
+
+// const sum = function(a,b){
+//     return a+b
+// }
+
+
+module.exports = getNotes
